4c28af820c693b865e63ce6f807e8d527793a192
adi dabush <adi.dabush@wework.com>
2019-07-12 21:35:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function teenager(age) {
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!"
-  } else if (age <= 19) {
+  } else if (age <= 20) {
     return "You are a grownup"
   } else {
     return "You are a kid"
